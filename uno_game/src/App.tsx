0a965ef0eb1a543d5454bdc2401b895b964ba9cf
DennisMat1 <dennismatheuwezen@gmail.com>
2023-03-23 02:57:09 +0100

    Uno game cards on numbers + colors

---
+++ uno_game/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import UnoGame from './Game';
+function App() {
+  return (
+    <div className="App">
+      <UnoGame/>
+    
+    </div>
+  );
+}
+
+export default App;
